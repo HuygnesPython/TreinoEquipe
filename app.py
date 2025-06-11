ccf2d689d662ea24364556b6baac53b56ea2ffbc
HuygnesPython <huygneszindocsgo@gmail.com>
2025-06-11 18:59:20 -0300

    Teste

---
+++ app.py
@@ -0,0 +1,22 @@
+import mysql.connector as my
+from pydantic import BaseModel
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# Ver shows
+
+
+
+
+# Criar shows
+
+
+
+
+# Deletar shows
+
+
+
+
+# Atualizar shows
