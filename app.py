78551cacc87a1e1914ed3b86f85eb4ea2ebc708b
HuygnesPython <huygneszindocsgo@gmail.com>
2025-06-11 19:15:57 -0300

    Teste

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
-import mysql.connector as my
-from pydantic import BaseModel
-from fastapi import FastAPI
-
-app = FastAPI()
+#_______________________________
+#Início                        |
+import mysql.connector as my#  |
+from pydantic import BaseModel#|
+from fastapi import FastAPI#   |
+def conectar_banco():#         |
+    conexao = my.conexao(#     |
+        host='localhost', #    |
+        user='root', #         |
+        password='',#          |
+        db='eventos_db'#       |
+    )#                         |
+    return conexao#            |
+app = FastAPI()#               |
+#------------------------------|
 
 # Ver shows
-
+@app.get('/VerShows')
+def ver_shows():
+    conexao = conectar_banco()
+    cursor = conexao.cursor()
+    sql = 'select * from show_db'
+    cursor.execute(sql)
+    resultado = cursor.fetchall()
+    conexao.close()
+    return resultado
 
 
 
@@ -20,3 +38,9 @@ app = FastAPI()
 
 
 # Atualizar shows
+
+
+
+
+# Ver um show
+
